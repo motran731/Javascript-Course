a1c4053fb6595e218b7edfc7a28d955a76a3b494
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-01 13:46:16 -0400

    dice image matches the dice number

--- PigGame/script.js
+++ PigGame/script.js
@@ -4,7 +4,24 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+//starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  console.log(dice);
+
+  //2. display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. check for rolled 1: if tru, switch to next player
+});
