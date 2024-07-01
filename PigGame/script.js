35357df986ab7c1ec11375739d2350c539693ef4
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-01 13:55:03 -0400

    dice number is added to player1's score if dice number is not 1

--- PigGame/script.js
+++ PigGame/script.js
@@ -3,6 +3,8 @@
 //selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +14,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -23,5 +27,12 @@ btnRoll.addEventListener('click', function () {
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
 
-  //3. check for rolled 1: if tru, switch to next player
+  //3. check for rolled 1: i
+  if (dice !== 1) {
+    //add ice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //TODO change later
+  } else {
+    //i f tru, switch to next player
+  }
 });
