4e54b1f14ff0928e962ce1f12e2eabdf4ed3db92
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-15 14:02:00 -0400

    same firstName but used in different scopes

--- 03_Behind_the_scenes/script.js
+++ 03_Behind_the_scenes/script.js
@@ -6,6 +6,20 @@ function calcAge(birthYear) {
   function printAge() {
     const output = ` ${firstName} you are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Alex';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    //cannot call cl(str) here
+    console.log(millenial);
+    console.log(add(2, 3)); //reference error unless you turn off strict mode
   }
   printAge();
   console.log(firstName);
