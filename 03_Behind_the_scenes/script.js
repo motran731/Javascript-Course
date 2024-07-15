484638b23f4a2a761777a4806f48f22fe11deea5
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-15 14:08:09 -0400

    reassigning outer scope's variable

--- 03_Behind_the_scenes/script.js
+++ 03_Behind_the_scenes/script.js
@@ -4,12 +4,16 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = ` ${firstName} you are ${age}, born in ${birthYear}`;
+    let output = ` ${firstName} you are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
+      //creating New variable with same name as outerscope's variable
       const firstName = 'Alex';
+
+      //reassigning outer scope's variable
+      output = 'New OUTPUT';
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
 
@@ -19,7 +23,8 @@ function calcAge(birthYear) {
     }
     //cannot call cl(str) here
     console.log(millenial);
-    console.log(add(2, 3)); //reference error unless you turn off strict mode
+    //console.log(add(2, 3)); //reference error unless you turn off strict mode
+    console.log(output);
   }
   printAge();
   console.log(firstName);
