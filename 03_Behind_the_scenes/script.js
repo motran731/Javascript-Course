25c51fb3fa43fdf540a9f8351a7dc2af8a42b40b
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-15 13:51:29 -0400

    printAge function

--- 03_Behind_the_scenes/script.js
+++ 03_Behind_the_scenes/script.js
@@ -2,6 +2,12 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = ` ${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
   console.log(firstName);
   return age;
 }
