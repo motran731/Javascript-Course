84652712c20c13be53296aa478d32403aea07fe5
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-30 10:59:10 -0400

    destructuring examples

--- 04_Data_Structures_operators/script.js
+++ 04_Data_Structures_operators/script.js
@@ -29,10 +29,38 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  // orderDelivery: function (obj) {
+  //   console.log(obj);
+  // },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '13:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
-//DESTRUCTURING OBJECTS
-//order of elements
+restaurant.orderDelivery({
+  time: '22:00',
+  address: 'via del, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+//using default values
+
+restaurant.orderDelivery({
+  address: 'via del, 21',
+  starterIndex: 1,
+});
+
+// NOTES DESTRUCTURING OBJECTS
+//1. order of elements does not matter
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
@@ -43,22 +71,49 @@ const {
 } = restaurant;
 console.log(restaurantName, hours, tags);
 
+//2. default values in objects
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//3. Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+//Nested objects
+// const { fri } = openingHours;
+// console.log(fri);
+
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
+
+//reassign different variables with :
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
 //DESTRUCTURING ARRAY
 //destructuring, left side of equal sign
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
 
 // const [first, , second] = restaurant.categories;
 // console.log(first, second);
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
 //1. switching variables
 // const temp = main;
@@ -73,270 +128,270 @@ console.log(main, secondary);
 //3. Receive 2 return values from a function, used destructure with order
 // console.log(restaurant.order(2, 0));
 
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
 
-//4. Nested destructuring array
-const nested = [2, 4, [5, 6]];
+// //4. Nested destructuring array
+// const nested = [2, 4, [5, 6]];
 
-// const [i, , j] = nested; // skip the 4
-// console.log(i, j);
+// // const [i, , j] = nested; // skip the 4
+// // console.log(i, j);
 
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
 
-//5. set default values when length of array is unknown
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+// //5. set default values when length of array is unknown
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
 
-//Destructuring Array assignment
-const books = [
-  {
-    title: 'Algorithms',
-    author: ['Robert Sedgewick', 'Kevin Wayne'],
-    publisher: 'Addison-Wesley Professional',
-    publicationDate: '2011-03-24',
-    edition: 4,
-    keywords: [
-      'computer science',
-      'programming',
-      'algorithms',
-      'data structures',
-      'java',
-      'math',
-      'software',
-      'engineering',
-    ],
-    pages: 976,
-    format: 'hardcover',
-    ISBN: '9780321573513',
-    language: 'English',
-    programmingLanguage: 'Java',
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.41,
-        ratingsCount: 1733,
-        reviewsCount: 63,
-        fiveStarRatingCount: 976,
-        oneStarRatingCount: 13,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'Structure and Interpretation of Computer Programs',
-    author: [
-      'Harold Abelson',
-      'Gerald Jay Sussman',
-      'Julie Sussman (Contributor)',
-    ],
-    publisher: 'The MIT Press',
-    publicationDate: '2022-04-12',
-    edition: 2,
-    keywords: [
-      'computer science',
-      'programming',
-      'javascript',
-      'software',
-      'engineering',
-    ],
-    pages: 640,
-    format: 'paperback',
-    ISBN: '9780262543231',
-    language: 'English',
-    programmingLanguage: 'JavaScript',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.36,
-        ratingsCount: 14,
-        reviewsCount: 3,
-        fiveStarRatingCount: 8,
-        oneStarRatingCount: 0,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: "Computer Systems: A Programmer's Perspective",
-    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
-    publisher: 'Prentice Hall',
-    publicationDate: '2002-01-01',
-    edition: 1,
-    keywords: [
-      'computer science',
-      'computer systems',
-      'programming',
-      'software',
-      'C',
-      'engineering',
-    ],
-    pages: 978,
-    format: 'hardcover',
-    ISBN: '9780130340740',
-    language: 'English',
-    programmingLanguage: 'C',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.44,
-        ratingsCount: 1010,
-        reviewsCount: 57,
-        fiveStarRatingCount: 638,
-        oneStarRatingCount: 16,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'Operating System Concepts',
-    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
-    publisher: 'John Wiley & Sons',
-    publicationDate: '2004-12-14',
-    edition: 10,
-    keywords: [
-      'computer science',
-      'operating systems',
-      'programming',
-      'software',
-      'C',
-      'Java',
-      'engineering',
-    ],
-    pages: 921,
-    format: 'hardcover',
-    ISBN: '9780471694663',
-    language: 'English',
-    programmingLanguage: 'C, Java',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 3.9,
-        ratingsCount: 2131,
-        reviewsCount: 114,
-        fiveStarRatingCount: 728,
-        oneStarRatingCount: 65,
-      },
-    },
-  },
-  {
-    title: 'Engineering Mathematics',
-    author: ['K.A. Stroud', 'Dexter J. Booth'],
-    publisher: 'Palgrave',
-    publicationDate: '2007-01-01',
-    edition: 14,
-    keywords: ['mathematics', 'engineering'],
-    pages: 1288,
-    format: 'paperback',
-    ISBN: '9781403942463',
-    language: 'English',
-    programmingLanguage: null,
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.35,
-        ratingsCount: 370,
-        reviewsCount: 18,
-        fiveStarRatingCount: 211,
-        oneStarRatingCount: 6,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'The Personal MBA: Master the Art of Business',
-    author: 'Josh Kaufman',
-    publisher: 'Portfolio',
-    publicationDate: '2010-12-30',
-    keywords: ['business'],
-    pages: 416,
-    format: 'hardcover',
-    ISBN: '9781591843528',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.11,
-        ratingsCount: 40119,
-        reviewsCount: 1351,
-        fiveStarRatingCount: 18033,
-        oneStarRatingCount: 1090,
-      },
-    },
-  },
-  {
-    title: 'Crafting Interpreters',
-    author: 'Robert Nystrom',
-    publisher: 'Genever Benning',
-    publicationDate: '2021-07-28',
-    keywords: [
-      'computer science',
-      'compilers',
-      'engineering',
-      'interpreters',
-      'software',
-      'engineering',
-    ],
-    pages: 865,
-    format: 'paperback',
-    ISBN: '9780990582939',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.7,
-        ratingsCount: 253,
-        reviewsCount: 23,
-        fiveStarRatingCount: 193,
-        oneStarRatingCount: 0,
-      },
-    },
-  },
-  {
-    title: 'Deep Work: Rules for Focused Success in a Distracted World',
-    author: 'Cal Newport',
-    publisher: 'Grand Central Publishing',
-    publicationDate: '2016-01-05',
-    edition: 1,
-    keywords: ['work', 'focus', 'personal development', 'business'],
-    pages: 296,
-    format: 'hardcover',
-    ISBN: '9781455586691',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.19,
-        ratingsCount: 144584,
-        reviewsCount: 11598,
-        fiveStarRatingCount: 63405,
-        oneStarRatingCount: 1808,
-      },
-    },
-    highlighted: true,
-  },
-];
+// //Destructuring Array assignment
+// const books = [
+//   {
+//     title: 'Algorithms',
+//     author: ['Robert Sedgewick', 'Kevin Wayne'],
+//     publisher: 'Addison-Wesley Professional',
+//     publicationDate: '2011-03-24',
+//     edition: 4,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'algorithms',
+//       'data structures',
+//       'java',
+//       'math',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 976,
+//     format: 'hardcover',
+//     ISBN: '9780321573513',
+//     language: 'English',
+//     programmingLanguage: 'Java',
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.41,
+//         ratingsCount: 1733,
+//         reviewsCount: 63,
+//         fiveStarRatingCount: 976,
+//         oneStarRatingCount: 13,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Structure and Interpretation of Computer Programs',
+//     author: [
+//       'Harold Abelson',
+//       'Gerald Jay Sussman',
+//       'Julie Sussman (Contributor)',
+//     ],
+//     publisher: 'The MIT Press',
+//     publicationDate: '2022-04-12',
+//     edition: 2,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'javascript',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 640,
+//     format: 'paperback',
+//     ISBN: '9780262543231',
+//     language: 'English',
+//     programmingLanguage: 'JavaScript',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.36,
+//         ratingsCount: 14,
+//         reviewsCount: 3,
+//         fiveStarRatingCount: 8,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "Computer Systems: A Programmer's Perspective",
+//     author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+//     publisher: 'Prentice Hall',
+//     publicationDate: '2002-01-01',
+//     edition: 1,
+//     keywords: [
+//       'computer science',
+//       'computer systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'engineering',
+//     ],
+//     pages: 978,
+//     format: 'hardcover',
+//     ISBN: '9780130340740',
+//     language: 'English',
+//     programmingLanguage: 'C',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.44,
+//         ratingsCount: 1010,
+//         reviewsCount: 57,
+//         fiveStarRatingCount: 638,
+//         oneStarRatingCount: 16,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Operating System Concepts',
+//     author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+//     publisher: 'John Wiley & Sons',
+//     publicationDate: '2004-12-14',
+//     edition: 10,
+//     keywords: [
+//       'computer science',
+//       'operating systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'Java',
+//       'engineering',
+//     ],
+//     pages: 921,
+//     format: 'hardcover',
+//     ISBN: '9780471694663',
+//     language: 'English',
+//     programmingLanguage: 'C, Java',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 3.9,
+//         ratingsCount: 2131,
+//         reviewsCount: 114,
+//         fiveStarRatingCount: 728,
+//         oneStarRatingCount: 65,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Engineering Mathematics',
+//     author: ['K.A. Stroud', 'Dexter J. Booth'],
+//     publisher: 'Palgrave',
+//     publicationDate: '2007-01-01',
+//     edition: 14,
+//     keywords: ['mathematics', 'engineering'],
+//     pages: 1288,
+//     format: 'paperback',
+//     ISBN: '9781403942463',
+//     language: 'English',
+//     programmingLanguage: null,
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.35,
+//         ratingsCount: 370,
+//         reviewsCount: 18,
+//         fiveStarRatingCount: 211,
+//         oneStarRatingCount: 6,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'The Personal MBA: Master the Art of Business',
+//     author: 'Josh Kaufman',
+//     publisher: 'Portfolio',
+//     publicationDate: '2010-12-30',
+//     keywords: ['business'],
+//     pages: 416,
+//     format: 'hardcover',
+//     ISBN: '9781591843528',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.11,
+//         ratingsCount: 40119,
+//         reviewsCount: 1351,
+//         fiveStarRatingCount: 18033,
+//         oneStarRatingCount: 1090,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Crafting Interpreters',
+//     author: 'Robert Nystrom',
+//     publisher: 'Genever Benning',
+//     publicationDate: '2021-07-28',
+//     keywords: [
+//       'computer science',
+//       'compilers',
+//       'engineering',
+//       'interpreters',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 865,
+//     format: 'paperback',
+//     ISBN: '9780990582939',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.7,
+//         ratingsCount: 253,
+//         reviewsCount: 23,
+//         fiveStarRatingCount: 193,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Deep Work: Rules for Focused Success in a Distracted World',
+//     author: 'Cal Newport',
+//     publisher: 'Grand Central Publishing',
+//     publicationDate: '2016-01-05',
+//     edition: 1,
+//     keywords: ['work', 'focus', 'personal development', 'business'],
+//     pages: 296,
+//     format: 'hardcover',
+//     ISBN: '9781455586691',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.19,
+//         ratingsCount: 144584,
+//         reviewsCount: 11598,
+//         fiveStarRatingCount: 63405,
+//         oneStarRatingCount: 1808,
+//       },
+//     },
+//     highlighted: true,
+//   },
+// ];
 
-// 1.1 Destructure the books array into two variables called firstBook and secondBook.
-const [firstBook, secondBook] = books;
-console.log(firstBook, secondBook);
+// // 1.1 Destructure the books array into two variables called firstBook and secondBook.
+// const [firstBook, secondBook] = books;
+// console.log(firstBook, secondBook);
 
-//1.2 Destructure the books array into a variable called thirdBook. You must skip the first two books.
+// //1.2 Destructure the books array into a variable called thirdBook. You must skip the first two books.
 
-const [, , thirdBook] = books;
-console.log(thirdBook);
-// 1.3 Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+// const [, , thirdBook] = books;
+// console.log(thirdBook);
+// // 1.3 Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
 
-const ratings = [
-  ['rating', 4.19],
-  ['ratingsCount', 144584],
-];
+// const ratings = [
+//   ['rating', 4.19],
+//   ['ratingsCount', 144584],
+// ];
 
-const [[, rating], [, ratingsCount]] = ratings;
-console.log(rating, ratingsCount);
+// const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
 
-//1.4 Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
-const ratingStars = [63405, 1808];
-// const [fiveStarRatings = 0, oneStarRatings = 0, threeStarRatings = 0] =
-//   ratingStars;
+// //1.4 Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+// const ratingStars = [63405, 1808];
+// // const [fiveStarRatings = 0, oneStarRatings = 0, threeStarRatings = 0] =
+// //   ratingStars;
 
-const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
-console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+// const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
